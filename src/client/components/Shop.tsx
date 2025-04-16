0ca888f6b067817f6fd94bd07e97401ce87fac8c
ethandoan <ethan.doan@wilink.de>
2025-04-16 16:35:06 -0500

    Shop Page

--- src/client/components/Shop.tsx
+++ src/client/components/Shop.tsx
@@ -1,12 +1,47 @@
 import "../styles/Shop.css";
 import Socials from "./Socials.tsx";
+import ShopItem from "./ShopItem.tsx";
+import CactusMug from "../media/CactusMug.jpeg";
+import GreenMugTall from "../media/GreenMugTall.jpeg";
+import PurpleBubbleVase from "../media/PurpleBubbleVase.jpeg";
+import ModernLineVase from "../media/ModernLineVase.jpeg";
+import BubblegumMug from "../media/BubblegumMug.jpeg";
+import MoodyBowl from "../media/MoodyBowl.jpeg";
 
 function Shop() {
     return (
         <div className="shop-container">
             <div className="shop-content">
-                Coming soon... In the meantime, please contact me via email for
-                orders or other inquiries
+                <ShopItem
+                    imageUrl={GreenMugTall}
+                    description="Dark Planter"
+                    price="$40.00"
+                />
+                <ShopItem
+                    imageUrl={CactusMug}
+                    description="Cactus Mug"
+                    price="$20.00"
+                />
+                <ShopItem
+                    imageUrl={PurpleBubbleVase}
+                    description="Purple Bubble Vase"
+                    price="$40.00"
+                />
+                <ShopItem
+                    imageUrl={ModernLineVase}
+                    description="Modern Line Vase"
+                    price="$40.00"
+                />
+                <ShopItem
+                    imageUrl={BubblegumMug}
+                    description="Bubblegum Mug"
+                    price="$25.00"
+                />
+                <ShopItem
+                    imageUrl={MoodyBowl}
+                    description="Moody Bowl"
+                    price="$45.00"
+                />
             </div>
             <Socials />
         </div>
---
+++ src/client/components/ShopItem.tsx
@@ -0,0 +1,32 @@
+import "../styles/ShopItem.css";
+import { Link } from "react-router-dom";
+
+type ShopItemProps = {
+    imageUrl: string;
+    description: string;
+    price: string;
+};
+
+function ShopItem({ imageUrl, description, price }: ShopItemProps) {
+    return (
+        <div className="shop-item-container">
+            <div className="shop-item">
+                <img
+                    src={imageUrl}
+                    alt={description}
+                    className="shop-item-image"
+                />
+                <div className="shop-item-content">
+                    <p className="shop-item-description">
+                        {description} | {price}
+                    </p>
+                    <button className="shop-item-buy">
+                        <Link to="/contact">Buy</Link>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ShopItem;
