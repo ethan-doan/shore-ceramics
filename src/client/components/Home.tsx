f20f68a5ad528ad26ce62f2b2e4cc22cb7349032
ethandoan <ethan.doan@wilink.de>
2025-04-30 21:53:33 -0500

    Fixed Typo, Added Email List

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -1,118 +1,104 @@
 import "../styles/Home.css";
+import { useRef, useState } from "react";
 import Socials from "./Socials.tsx";
+import emailjs from "@emailjs/browser";
 
 function Home() {
-  return (
-    <div className="home-container fade-in">
-      <div className="home-image-array">
-        <img
-          className="home-image"
-          src="/media/PurpleMug.jpg"
-          alt="purple mug"
-        />
-        <img className="home-image" src="/media/RedVase.jpg" alt="red vasel" />
-        <img
-          className="home-image"
-          src="/media/GreenMugTall.jpg"
-          alt="green planter"
-        />
-      </div>
-      <div className="home-about-me">
-        <div className="home-about-me-content">
-          <div className="home-about-me-header">Handmade Pottery</div>
-          <div className="home-about-me-description">
-            Hi, I’m Lauren and I’ve been doing pottery for as long as I can
-            remember. I started in art class in elementary school, and really
-            focused on throwing on the wheel in high school. Now that I’m in my
-            twenties, I’ve rediscovered my passion for ceramics! Take a look
-            around at my recent work and contact me about commissions or to
-            purchase a piece. Currently hand delivery in Omaha, and shipping
-            throughout the US.
-          </div>
+    const formRef = useRef<HTMLFormElement>(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!formRef.current) {
+            return;
+        }
+
+        setLoading(true);
+
+        emailjs
+            .sendForm(
+                "service_rdjy9ce",
+                "template_bvcc7cp",
+                formRef.current,
+                "MqPxgpxuq6ZHXMigo"
+            )
+            .then(
+                (result: EmailJSResponseStatus) => {
+                    console.log("Email sent succesfully!", result.text);
+                    formRef.current?.reset();
+                },
+                (error: any) => {
+                    console.error("Failed to send email:", error.text);
+                }
+            )
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+    return (
+        <div className="home-container fade-in">
+            <div className="home-image-array">
+                <img
+                    className="home-image"
+                    src="/media/PurpleMug.jpg"
+                    alt="purple mug"
+                />
+                <img
+                    className="home-image"
+                    src="/media/RedVase.jpg"
+                    alt="red vasel"
+                />
+                <img
+                    className="home-image"
+                    src="/media/GreenMugTall.jpg"
+                    alt="green planter"
+                />
+            </div>
+            <div className="home-about-me">
+                <div className="home-about-me-content">
+                    <div className="home-about-me-header">Handmade Pottery</div>
+                    <div className="home-about-me-description">
+                        Hi, I’m Lauren and I’ve been doing pottery for as long
+                        as I can remember. I started in art class in elementary
+                        school, and really focused on throwing on the wheel in
+                        high school. Now that I’m in my twenties, I’ve
+                        rediscovered my passion for ceramics! Take a look around
+                        at my recent work and contact me about commissions or to
+                        purchase a piece. Currently offering hand delivery in
+                        Omaha, and shipping throughout the US.
+                    </div>
+                </div>
+            </div>
+            <form
+                ref={formRef}
+                className="contact-form"
+                onSubmit={handleSubmit}
+            >
+                <div className="home-mailing-list">
+                    <div className="home-mailing-list-header">
+                        Stay in the loop!
+                    </div>
+                    <div className="home-mailing-list-description">
+                        Be the first to know about new collections, upcoming
+                        shows, and more.
+                    </div>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        className="home-mailing-list-input"
+                        required
+                    />
+                    <button className="home-mailing-list-submit">
+                        {loading ? "Sending..." : "Sign Up"}
+                    </button>
+                </div>
+            </form>
+            <Socials />
         </div>
-        {/* <div className="home-about-me-tiktok">
-                    <blockquote
-                        className="tiktok-embed"
-                        cite="https://www.tiktok.com/@shoreceramics/video/7492949399413345567"
-                        data-video-id="7492949399413345567"
-                        style={{ maxWidth: "605px", minWidth: "325px" }}
-                    >
-                        <section>
-                            <a
-                                target="_blank"
-                                title="@shoreceramics"
-                                href="https://www.tiktok.com/@shoreceramics?refer=embed"
-                            >
-                                @shoreceramics
-                            </a>
-                            big ole planter 6lbs{" "}
-                            <a
-                                title="pottery"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/pottery?refer=embed"
-                            >
-                                #pottery
-                            </a>
-                            <a
-                                title="ceramics"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/ceramics?refer=embed"
-                            >
-                                #ceramics
-                            </a>
-                            <a
-                                title="potterytiktok"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/potterytiktok?refer=embed"
-                            >
-                                #potterytiktok
-                            </a>
-                            <a
-                                title="potterywheel"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/potterywheel?refer=embed"
-                            >
-                                #potterywheel
-                            </a>
-                            <a
-                                title="clay"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/clay?refer=embed"
-                            >
-                                #clay
-                            </a>
-                            <a
-                                title="planter"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/planter?refer=embed"
-                            >
-                                #planter
-                            </a>
-                            <a
-                                title="plantsoftiktok"
-                                target="_blank"
-                                href="https://www.tiktok.com/tag/plantsoftiktok?refer=embed"
-                            >
-                                #plantsoftiktok
-                            </a>
-                            <a
-                                target="_blank"
-                                title="♬ オリジナル楽曲  - ライディーン"
-                                href="https://www.tiktok.com/music/オリジナル楽曲-ライディーン-7343966047912069889?refer=embed"
-                            >
-                                ♬ オリジナル楽曲 - ライディーン
-                            </a>
-                        </section>
-                    </blockquote>
-                    <script
-                        async
-                        src="https://www.tiktok.com/embed.js"
-                    ></script>
-                </div> */}
-      </div>
-      <Socials />
-    </div>
-  );
+    );
 }
 
 export default Home;
