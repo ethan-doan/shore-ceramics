0184292ad8f1315306d58e7be2881a9a8e862009
ethandoan <ethan.doan@wilink.de>
2025-05-04 22:38:39 -0500

    Phone number syntax wrapping and alert banner on contact me page

--- src/client/components/Contact.tsx
+++ src/client/components/Contact.tsx
@@ -1,4 +1,5 @@
 import "../styles/Contact.css";
+import { SubmissionStatus } from "../enums/SubmissionStatus.ts";
 import { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { EmailJSResponseStatus } from "@emailjs/browser";
@@ -6,7 +7,10 @@ import Socials from "./Socials.tsx";
 
 function Contact() {
     const formRef = useRef<HTMLFormElement>(null);
-    const [loading, setLoading] = useState(false);
+    const [phone, setPhone] = useState("");
+    const [status, setStatus] = useState<SubmissionStatus>(
+        SubmissionStatus.None
+    );
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -14,8 +18,6 @@ function Contact() {
         if (!formRef.current) {
             return;
         }
-
-        setLoading(true);
 
         emailjs
             .sendForm(
@@ -27,20 +29,49 @@ function Contact() {
             .then(
                 (result: EmailJSResponseStatus) => {
                     console.log("Email sent succesfully!", result.text);
+                    setStatus(SubmissionStatus.Success);
                     formRef.current?.reset();
+                    setPhone("");
                 },
                 (error: any) => {
                     console.error("Failed to send email:", error.text);
+                    setStatus(SubmissionStatus.Error);
                 }
-            )
-            .finally(() => {
-                setLoading(false);
-            });
+            );
+    };
+
+    const formatPhone = (value) => {
+        const digits = value.replace(/\D/g, "").slice(0, 10);
+        const area = digits.slice(0, 3);
+        const middle = digits.slice(3, 6);
+        const ending = digits.slice(6, 10);
+
+        if (digits.length > 6) {
+            return `(${area}) ${middle}-${ending}`;
+        } else if (digits.length > 3) {
+            return `(${area}) ${middle}`;
+        } else if (digits.length > 0) {
+            return `(${area}`;
+        }
+        return "";
     };
 
     return (
         <div className="contact-container fade-in">
             <div className="contact-header">Contact Me</div>
+            {status !== SubmissionStatus.None && (
+                <div
+                    className={`alert-banner ${
+                        status === SubmissionStatus.Success
+                            ? "alert-success"
+                            : "alert-error"
+                    }`}
+                >
+                    {status === SubmissionStatus.Success
+                        ? "Email sent successfully!"
+                        : "Failed to send email. Please try again."}
+                </div>
+            )}
             <div className="contact-description">
                 Have inquiries about my collection or a commission? Shoot me an
                 email and we can chat! If you are interested in a specific
@@ -61,14 +92,19 @@ function Contact() {
                 <input
                     type="tel"
                     name="phone"
-                    pattern="^\+?[0-9\s\-]{7,15}$"
+                    value={phone}
+                    pattern="^\(\d{3}\) \d{3}-\d{4}$"
+                    onChange={(e) => {
+                        setPhone(formatPhone(e.target.value));
+                    }}
                     placeholder="(123) 456-7890"
                     className="form-input"
                     required
                 />
                 <input
-                    type="text"
+                    type="email"
                     name="email"
+                    inputMode="email"
                     placeholder="yourname@example.com"
                     className="form-input"
                     required
@@ -81,7 +117,7 @@ function Contact() {
                     required
                 />
                 <button className="contact-submit" tabIndex={0} type="submit">
-                    {loading ? "Sending" : "Send"}
+                    Send
                 </button>
             </form>
             <Socials />
---
+++ src/client/enums/SubmissionStatus.ts
@@ -0,0 +1,5 @@
+export enum SubmissionStatus {
+    Success = "Success",
+    Error = "Error",
+    None = "None",
+}
